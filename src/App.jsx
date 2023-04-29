505020fccfaa95f9770db58446fa1d642703ecfc
Yonis7 <yonisaden7@gmail.com>
2023-04-29 18:32:09 -0400

    Completed Nav and Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import Header from "./components/header/header";
+import Hero from "./components/hero/hero";
+import "./App.css";
 
 function App() {
   return (
-    <span>subscribe Zainkeepscode</span>
+    <div className="App">
+      <div>
+        <div className="white-gradient"/>
+
+      <Header/>
+      <Hero/>
+      </div>
+    </div>
   );
 }
 
