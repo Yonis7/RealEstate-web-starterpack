9f6fd1d3a702857dc3893c414f02b689d9154a5a
Yonis7 <yonisaden7@gmail.com>
2023-06-16 02:50:30 -0400

    Value Section Complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/hero/hero";
 import "./App.css";
 import Companies from "./components/Companies/Companies";
 import Residenceis from "./components/Residencies/Residenceis";
+import Value from "./components/Value/Value";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       </div>
       <Companies/>
       <Residenceis/>
+      <Value/>
     </div>
   );
 }
